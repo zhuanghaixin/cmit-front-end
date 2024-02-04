b2fac2d1cd24d76123526bbd75b77b701460fb20
zhuanghaixin <v_haxzhuang@tencent.com>
2024-02-04 23:40:25 +0800

    feat: 初始化

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -7,15 +7,34 @@ import { t } from '/@/hooks/web/useI18n'
 
 // import.meta.globEager() 直接引入所有的模块 Vite 独有的功能
 const modules = import.meta.globEager('./modules/**/*.ts')
+console.log('modules', modules)
 const routeModuleList: AppRouteModule[] = []
 
 // 加入到路由集合中
+// todo 为什么要这么写？ 不是和原来写法一样吗？
+// const fullfillModules=(
+//   modules:Record<string,
+//   {
+//     [key:string]:any
+//   }
+//   >,
+//   list:AppRouteModule[]
+// )=>{
+//   Object.keys(modules).forEach((key) => {
+//     const mod = modules[key].default || {}
+//     const modList = Array.isArray(mod) ? [...mod] : [mod]
+//     routeModuleList.push(...modList)
+//   })
+// }
+
+// fullfillModules(modules,routeModuleList)
+
 Object.keys(modules).forEach((key) => {
   const mod = modules[key].default || {}
   const modList = Array.isArray(mod) ? [...mod] : [mod]
   routeModuleList.push(...modList)
 })
-
+console.log('routeModuleList', routeModuleList)
 export const asyncRoutes = [PAGE_NOT_FOUND_ROUTE, ...routeModuleList]
 
 // 根路由
